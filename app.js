bda8736cb7db1e7ac59e11e64696702fcd6bbe72
lamour <guiboguizo9@gmail.com>
2024-03-23 14:57:16 -0400

    update app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+// JavaScript code to display "Welcome to America" to any IP address
+
+// Function to check if the IP address belongs to America
+function isAmericanIP(ip) {
+    // Assuming IP addresses starting with "192.168." belong to America
+    if (ip.startsWith("192.168.")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Function to display welcome message based on IP address
+function displayWelcomeMessage(ip) {
+    if (isAmericanIP(ip)) {
+        console.log("Welcome to America");
+    } else {
+        console.log("You are not in America");
+    }
+}
+
+// Example usage:
+var ipAddress = "192.168.1.1"; // Replace this with any IP address you want to check
+displayWelcomeMessage(ipAddress);
